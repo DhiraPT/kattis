203b18f2aa9e58badf4c53b5be4672ba61ba4868
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2023-10-21 05:56:50 +0800

    Removed an extra space from FastIO Because I'm a perfectionist :)

--- weakvertices/weakvertices.java
+++ weakvertices/weakvertices.java
@@ -11,7 +11,7 @@ class FastIO extends PrintWriter {
     String next() {
         while (st == null || ! st.hasMoreElements()) {
             try { st = new StringTokenizer(br.readLine()); }
-            catch (IOException  e) { e.printStackTrace(); }
+            catch (IOException e) { e.printStackTrace(); }
         }
         return st.nextToken();
     }
