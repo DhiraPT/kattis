5bf71c886090d49e5aae417242677e4089e1c317
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2023-10-21 04:13:38 +0800

    Changed TreeMap into array

--- traveltheskies/traveltheskies.java
+++ traveltheskies/traveltheskies.java
@@ -31,26 +31,26 @@ public class traveltheskies {
         FastIO fio = new FastIO();
         int k = fio.nextInt(), n = fio.nextInt(), m = fio.nextInt();
         int[] airports = new int[k];
-        TreeMap<Integer, ArrayList<int[]>> flights = new TreeMap<>();
-        TreeMap<Integer, ArrayList<int[]>> arrivals = new TreeMap<>();
-        for (int i = 1; i <= n; i++) {
-            flights.put(i, new ArrayList<>());
-            arrivals.put(i, new ArrayList<>());
+        List<int[]>[] flights = new ArrayList[n];
+        List<int[]>[] arrivals = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            flights[i] = new ArrayList<>();
+            arrivals[i] = new ArrayList<>();
         }
         while (m-- > 0) {
             int u = fio.nextInt(), v = fio.nextInt(), d = fio.nextInt(), z = fio.nextInt();
-            flights.get(d).add(new int[]{u, v, z});
+            flights[d-1].add(new int[]{u, v, z});
         }
         for (int j = 0; j < k*n; j++) {
             int a = fio.nextInt(), b = fio.nextInt(), c = fio.nextInt();
-            arrivals.get(b).add(new int[]{a, c});
+            arrivals[b-1].add(new int[]{a, c});
         }
         boolean isOptimal = true;
-        outer: for (int s = 1; s <= n; s++) {
-            for (int[] arrival: arrivals.get(s)) {
+        outer: for (int s = 0; s < n; s++) {
+            for (int[] arrival: arrivals[s]) {
                 airports[arrival[0]-1] += arrival[1];
             }
-            for (int[] flight: flights.get(s)) {
+            for (int[] flight: flights[s]) {
                 if (flight[2] > airports[flight[0]-1]) {
                     fio.println("suboptimal");
                     isOptimal = false;
@@ -58,7 +58,7 @@ public class traveltheskies {
                 }
                 airports[flight[0]-1] -= flight[2];
             }
-            for (int[] flight: flights.get(s)) {
+            for (int[] flight: flights[s]) {
                 airports[flight[1]-1] += flight[2];
             }
         }
